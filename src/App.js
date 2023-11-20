b6f14cd8d27740fa29847fadf25d6466b4b16fee
fascilio <fascilior@gmail.com>
2023-11-20 19:12:46 +0300

    header section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
+import Header from './components/Head/Header'
+import "./App.css"
 
 const App = () => {
-  return (
-    <div>
-      <h1>Hello react</h1>
-    </div>
-  )
+  return <Header />
 }
 
 export default App
